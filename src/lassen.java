497594535a480e2533aecd3ba7d3ec72e512ea19
Лера <lera290897@gmail.com>
2022-11-06 15:29:45 +0300

    lassen 2.1

---
+++ src/lassen.java
@@ -0,0 +1,10 @@
+public class lassen {
+    public static void main(String[] args) {
+
+        int price = 5000; // Цена за один билет
+        int rub = 20; // Количество рублей для одной бонусной мили
+        System.out.println(price / rub + " бонусных миль "); // Количество бонусных миль за билет
+
+
+    }
+}
